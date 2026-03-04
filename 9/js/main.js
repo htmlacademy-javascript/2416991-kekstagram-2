3de617ce9d9e24e9610ba1a01d46bffb0f11152e
Keks <keks@htmlacademy.ru>
2026-03-04 12:23:27 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,7 @@
+import { renderGallery } from './gallery/gallery.js';
+import { initDetails } from './photo-details/photo-details.js';
+import { initForm } from './form/form.js';
+
+initDetails();
+renderGallery();
+initForm();
